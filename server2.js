2bcc5c7bf36b88e3a0b34dfd36a1e6b34afa2421
Brilyandika Andhana <brilyandika.andhana@gmail.com>
2019-01-07 10:14:53 +0700

    Coba Query

---
+++ server2.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
+    }
+`);
+
+var coursesData = [ //isi data
+    {
+        id: 1,
+        title: 'The Complete Node.js Developer Course',
+        author: 'Andrew Mead, Rob Percival',
+        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        topic: 'Node.js',
+        url: 'https://codingthesmartway.com/courses/nodejs/'
+    },
+    {
+        id: 2,
+        title: 'Belajar Jadi MasterChef',
+        author: 'Brilyandika Rizky Andhana',
+        description: 'Anda akan diedukasi tentang cara menjadi avatar,eh bukan masterchef',
+        topic: 'Food',
+        url: 'https://cookpad.com/id'
+    },
+        {
+        id: 3,
+        title: 'JavaScript: Understanding The Weird Parts',
+        author: 'Ivan Fadila, Aka',
+        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        topic: 'JavaScript',
+        url: 'https://www.udemy.com/the-complete-javascript-course/'
+    }
+]
+
+var getCourse = function(args) { 
+    var id = args.id;
+    return coursesData.filter(course => {
+        return course.id == id;
+    })[0];
+}
+var getCourses = function(args) {
+    if (args.topic) {
+        var topic = args.topic;
+        return coursesData.filter(course => course.topic === topic);
+    } else {
+        return coursesData;
+    }
+}
+
+var root = {
+    course: getCourse,
+    courses: getCourses
+};
+
+
+// Buat server dan endpoint
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+
